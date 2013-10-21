4e2c833de25e2c70028b56fc5bf8ce5e9bb7137d
Calvin Flegal <calvin.flegal@gmail.com>
2013-10-21 18:43:05 -0400

    a bit closer?

--- onCar-Challenge4/src/org/sunspotworld/URange.java
+++ onCar-Challenge4/src/org/sunspotworld/URange.java
@@ -47,7 +47,13 @@ public class URange extends MIDlet {
         private static final int SERVO2_HIGH = 10; //speeding step high
     private static final int SERVO2_LOW = 5; //speeding step low
         private static final int SERVO2_MAX_VALUE = 2000;
-        private static final int SERVO2_MIN_VALUE = 1500;
+        private static final int SERVO2_MIN_VALUE = 1530;
+         private int current1 = SERVO_CENTER_VALUE;
+         private static final int SERVO1_MAX_VALUE = 1800;
+    private static final int SERVO1_MIN_VALUE = 1400;
+    private static final int SERVO1_HIGH = 20; //steering step high
+    private static final int SERVO1_LOW = 10; //steering step low
+    private int step1 = SERVO1_LOW;
     
     public static final double voltage = 5.0; 
     public static final double scaleFactor = voltage/512;
@@ -74,7 +80,6 @@ public class URange extends MIDlet {
         
 
    while(true){
-      forward();
        try {
             try {
             // Open up a broadcast connection to the host port
@@ -89,6 +94,16 @@ public class URange extends MIDlet {
            //    Utils.sleep(250);
                 double inchesCarRight = (rightCarSensor.getVoltage()/scaleFactor);
                 double inchesCarLeft = (leftCarSensor.getVoltage())/scaleFactor;
+                if (inchesCarRight < 35) {
+                    right();
+                }
+                else if (inchesCarLeft < 35) {
+                    left();
+                }
+                else {
+                    forward();
+                    System.out.println("Forward");
+                }
                 dg.reset();
                 //enoc
                 dg.writeDouble(inchesCarLeft);
@@ -99,7 +114,6 @@ public class URange extends MIDlet {
             } catch (IOException ex){
                 ex.printStackTrace();
             }
-        
         }
      }
 
@@ -125,11 +139,11 @@ public class URange extends MIDlet {
             if (current2 -step2>SERVO2_MIN_VALUE){
                 servo2.setValue(current2-step2);
                 System.out.println(servo2.getValue());
-                Utils.sleep(50);
+               Utils.sleep(100);
             } else {
                 servo2.setValue(SERVO2_MIN_VALUE);
                 System.out.println(servo2.getValue());
-                Utils.sleep(50);
+                Utils.sleep(100);
             }
         } 
     /*    while (current2 - step2 > SERVO2_MIN_VALUE){
@@ -137,5 +151,28 @@ public class URange extends MIDlet {
         current2 = servo2.getValue();
         Utils.sleep(50);
                 }*/
+    }
+     private void left() {
+        System.out.println("left");
+        current1 = servo1.getValue();
+        if (current1 + step1 < SERVO1_MAX_VALUE){
+            servo1.setValue(current1+step1);
+          //  Utils.sleep(50);
+        } else {
+            servo1.setValue(SERVO1_MAX_VALUE);
+          //  Utils.sleep(50);
+        }
+    }
+     private void right() {
+        System.out.println("right");
+        current1 = servo1.getValue();
+        if (current1-step1 > SERVO1_MIN_VALUE){
+            servo1.setValue(current1-step1);
+           // Utils.sleep(50);
+        } else {
+            servo1.setValue(SERVO1_MIN_VALUE);
+           // Utils.sleep(50);
+        }
+        //servo2.setValue(0);
     }
 }
